0cf0a4b850a620034a477fa8a4f2fc2e8da056d8
zanmato1984 <zanmato1984@gmail.com>
2021-01-16 14:09:21 +0800

    Add setting sqls

---
+++ sql/cpu_settings.sql
@@ -0,0 +1,2 @@
+set tidb_mem_quota_query = 343597383681;
+set tidb_executor_concurrency = 40;
---
+++ sql/gpu_setting.sql
@@ -0,0 +1,12 @@
+set tidb_mem_quota_query = 343597383681;
+
+set tidb_cura_chunk_size = 4100000;
+set tidb_enable_cura_exec = 1;
+set tidb_cura_support = 127;
+set tidb_cura_concurrent_input_source = 0;
+
+set tidb_cura_mem_res_type = arena;
+set tidb_cura_mem_res_size = 7000 * 1024 * 1024;
+set tidb_cura_exclusive_default_mem_res = 1;
+
+set tidb_cura_stream_concurrency = 3;
